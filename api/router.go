4d9257b34930f482728872fa5e75249ebd4dc400
mananispiwpiw <rafijaeger@gmail.com>
2024-03-20 05:59:35 +0700

    update and add new route with new router features (adding method directly)

--- api/router.go
+++ api/router.go
@@ -8,5 +8,6 @@ func NewRouter() {
 	//Handler thingy
 	http.HandleFunc("/tasks", GetTasksHandler)
 	http.HandleFunc("/task", AddTaskHandler)
-	http.HandleFunc("/task/{id}", DeleteTaskHandler)
+	http.HandleFunc("DELETE /task/{id}", DeleteTaskHandler)
+	http.HandleFunc("PUT /task/{id}", UpdateTaskHandler)
 }
