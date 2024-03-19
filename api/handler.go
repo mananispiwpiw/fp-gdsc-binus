8fe6b48c137de73b30bad108a4dae994460ab727
mananispiwpiw <rafijaeger@gmail.com>
2024-03-19 22:34:49 +0700

    create handler for adding user

--- api/handler.go
+++ api/handler.go
@@ -1,7 +1,9 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/mananispiwpiw/fp-gdsc-binus/db"
 )
@@ -22,4 +24,32 @@ func GetUsersHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
+}
+
+// AddUser funcction
+func AddUserHandler(w http.ResponseWriter, r *http.Request) {
+	//Chcek if the request method is not POST
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Parse and decode the request body
+	var user db.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Add the user to the database
+	err = db.AddUser(user.ID, user.Username, user.Password, time.Now(), time.Now())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Write the response
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(user)
 }
